03ffa741569b6a2909efef7fffda7f113e059e91
Robin Hilliard <rhilliard@bitbucket.org>
2025-06-15 22:51:18 +1000

    Update .formatter.exs to restrict inputs for formatting
    - Disable code formatting - matrix-type layouts for lists/tuples are far too important for readability in graphics programming

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,5 @@
-# Used by "mix format"
+# Disable formatting for code - matrix layouts for lists are
+# far too important for readability in graphics programming
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config}/**/*.{ex,exs}"]
 ]
