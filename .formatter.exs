9df21ae1d34fde8140672322fae56b12fcb6fc0f
Robin Hilliard <rhilliard@bitbucket.org>
2025-06-16 00:10:28 +1000

    Update .formatter.exs to include lib and test directories in formatting inputs

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,3 @@
-# Disable formatting for code - matrix layouts for lists are
-# far too important for readability in graphics programming
 [
-  inputs: ["{mix,.formatter}.exs", "{config}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
 ]
